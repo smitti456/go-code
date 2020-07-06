b86cd74b42ba9caf2baec991a33cc7cea936a263
Smitti456 <smittis@gmail.com>
2020-07-06 11:37:49 +0700

    first commit

---
+++ controllers/test1.go
@@ -0,0 +1,9 @@
+package controllers
+
+import (
+	"fmt"
+)
+
+func Hi() {
+	fmt.Printf("Hi I'm Smitti")
+}
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/smitti456/go-code/controllers"
+)
+
+func main() {
+	fmt.Printf("Hello test go-code")
+	controllers.Hi()
+}
